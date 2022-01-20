0edb4fe6025e3709dd97f2ffe4b52004484c0c4e
Wafa Mohessin <wafamohessin@gmail.com>
2022-01-20 08:55:28 -0500

    routes api.js

---
+++ seeds/seed.js
@@ -0,0 +1,146 @@
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+const mongoose = require('mongoose');
+const db = require('../models');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('connect to database from seeder')
+});
+
+const workoutSeed = [
+
+    {
+        day: new Date(new Date().setDate(new Date().getDate() - 6)),
+        exercises: [
+          {
+            type: 'cardio',
+            name: 'Running',
+            duration: 25,
+            distance: 4,
+          },
+        ],
+      },
+
+
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 9)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Bicep Curl',
+        duration: 20,
+        weight: 100,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 8)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: ' Pull Up',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 7)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Push Press',
+        duration: 25,
+        weight: 185,
+        reps: 8,
+        sets: 4,
+      },
+    ],
+  },
+
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 5)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Bench Press',
+        duration: 20,
+        weight: 285,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 3)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Quad Press',
+        duration: 30,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 4)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Bench Press',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 2)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Bench Press',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 1)),
+    exercises: [
+      {
+        type: 'resistance',
+        name: 'Military Press',
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4,
+      },
+    ],
+  },
+];
+
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then((data) => {
+    console.log(data.result.n + ' records inserted!');
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
